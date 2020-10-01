996c39c87a0986561f1d525f3ee3bb5a15d02ad9
David Thomas <ThomDavi727@GoTVDSB.ca>
2020-10-01 09:04:14 -0400

    Added most of the code

--- Part 3 Hypotenuse/Program.cs
+++ Part 3 Hypotenuse/Program.cs
@@ -8,6 +8,19 @@ namespace Part_3_Hypotenuse
         {
             double leg1;
             double leg2;
+            double hypotenuse;
+            
+
+
+            Console.WriteLine("Enter first leg length: ");
+            leg1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second leg length: ");
+            leg2 = Convert.ToDouble(Console.ReadLine());
+
+            hypotenuse = 
+
+            Console.WriteLine("The hypotenuse is: " + hypotenuse);
         }
     }
 }
