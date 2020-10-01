cea02153d156159110409cce8a2813eda0a2a2d1
David Thomas <ThomDavi727@GoTVDSB.ca>
2020-10-01 10:12:11 -0400

    Changes made

--- Part 3 Hypotenuse/Program.cs
+++ Part 3 Hypotenuse/Program.cs
@@ -9,7 +9,7 @@ namespace Part_3_Hypotenuse
             double leg1;
             double leg2;
             double hypotenuse;
-            static double Sqrt(double d);
+            static extern double Sqrt(double d);
 
             Console.WriteLine("Enter first leg length: ");
             leg1 = Convert.ToDouble(Console.ReadLine());
@@ -17,9 +17,10 @@ namespace Part_3_Hypotenuse
             Console.WriteLine("Enter second leg length: ");
             leg2 = Convert.ToDouble(Console.ReadLine());
 
-            (Math.Sqrt(leg2));
+            hypotenuse = (Math.Sqrt(leg1 * 2) + (leg2 * 2));
 
             Console.WriteLine("The hypotenuse is: " + hypotenuse);
+            Console.ReadLine();
         }
     }
 }
